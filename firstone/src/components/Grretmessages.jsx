ecf68f70430cac75fcbe1108fc7ce2f842697b31
harishvutukuru <harishvutukuru01@gmail.com>
2023-06-01 20:35:07 -0500

    commit3

---
+++ firstone/src/components/Grretmessages.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Greetmessage.css";
+const Grretmessages = () => {
+   
+  return (
+    <div>
+<div className='red'>Grretmessages</div>
+<div className='my-card'>
+    <h3>
+        Good Morning
+    </h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sunt ea illo sequi et dolor quam sit? Blanditiis, sequi minima laudantium tenetur cupiditate, odio sed nulla deleniti quas modi, fugit ad est provident fuga sit ipsam illum libero quae dicta!
+    </p>
+</div>
+    </div>
+    
+  )
+}
+
+export default Grretmessages 
